5c59946bb3665859242c9dfac8c47c9b19092d57
Stefan Geisbacher <s.geisbacher@scotty.group>
2026-03-10 06:08:28 +0100

    added first test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let query = "h l f";
     let text = "hello world, from stefan in graz!";
@@ -13,13 +15,43 @@ fn main() {
 }
 
 fn fuzzy_find(line: &str, query_parts: Vec<&str>) -> Vec<usize> {
-    let mut indexes = Vec::new();
+    let mut indexes = HashSet::new();
     for query in query_parts {
         if let Some(idx) = line.find(query) {
-            indexes.push(idx)
+            indexes.insert(idx);
         } else {
             return vec![];
         }
     }
-    indexes
+    indexes.into_iter().collect()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_fuzzy_find() {
+        let cases = [
+            (
+                "simple search",
+                "hello world, from europe!",
+                vec!["h", "l", "f"],
+                vec![0, 2, 13],
+            ),
+            (
+                "duplicated needle",
+                "hello world, from europe!",
+                vec!["h", "l", "f", "f"],
+                vec![0, 2, 13],
+            ),
+        ];
+
+        for (descr, line, needles, expected) in cases {
+            let mut result = fuzzy_find(line, needles);
+            result.sort_unstable();
+            assert_eq!(result, expected, "failed: {}", descr);
+        }
+    }
 }
